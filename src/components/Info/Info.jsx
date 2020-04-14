546078910a37022eb13b880eb514d040cb9701b3
Gursewak Singh <gursewak19d@gmail.com>
2020-04-14 20:29:53 +0530

    Info page on development

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+// import { Media } from "../Media/Media";
+import video from "../../videos/covid19.mp4";
+import style from "./Info.module.css";
 function Info() {
-  return <div>Info page</div>;
+  return (
+    <div className={style.container}>
+      <video autoplay="" controls loop width="100%" className={style.video}>
+        <source src={video} type="video/mp4" />
+      </video>
+    </div>
+  );
 }
 
 export default Info;
--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Card, CardMedia } from "@material-ui/core";
+import video from "../../videos/covid19.mp4";
 
 function Media() {
   return (
     <Card>
-      <CardMedia src="" />
+      <CardMedia src={video} />
     </Card>
   );
 }
